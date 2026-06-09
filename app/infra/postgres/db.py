ae2df52fad17d20446c5032407c3e06d5f91399d
larsanik <larsanik@gmail.com>
2026-06-09 16:07:42 +0300

    Разработайте административную панель. Шаг 4. Создайте приложение для административной панели.

--- app/infra/postgres/db.py
+++ app/infra/postgres/db.py
@@ -9,16 +9,20 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Database:
     def __init__(self, dsn: Secret[PostgresDsn], declarative_base: DeclarativeBase) -> None:
-        self._engine = create_async_engine(str(dsn.get_secret_value()))
-        self._async_session = async_sessionmaker(bind=self._engine)
+        """
+
+        :rtype: None
+        """
+        self.engine = create_async_engine(str(dsn.get_secret_value()))
+        self._async_session = async_sessionmaker(bind=self.engine)
 
         self._declarative_base = declarative_base
 
     async def shutdown(self) -> None:
-        await self._engine.dispose()
+        await self.engine.dispose()
 
     async def create_tables(self) -> None:
-        async with self._engine.begin() as conn:
+        async with self.engine.begin() as conn:
             await conn.run_sync(self._declarative_base.metadata.create_all)
 
     @asynccontextmanager
